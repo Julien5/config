66b45fe2d55ca312d26b211419fbf0c160ddd30f
Julien Bourgeois <julien_bourgeois@yahoo.fr>
2020-07-03 21:50:43 +0200

    * colors

--- emacs.d/msys/colors.el
+++ emacs.d/msys/colors.el
@@ -13,4 +13,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Courier" :slant normal :weight normal :height 120 :width normal :antialias true)))))
+ '(default ((t (:family "Courier New" :slant normal :weight normal :height 120 :width normal :antialias true :foundry "outline")))))
+ 
