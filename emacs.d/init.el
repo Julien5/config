5e34baac12d13c297f356dc33568c7a3eb51974d
Julien5 <julien_bourgeois@yahoo.fr>
2020-07-03 21:09:14 +0200

    linux port

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -81,7 +81,7 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Courier New" :foundry "outline" :slant normal :weight normal :height 120 :width normal :antialias true)))))
+ '(default ((t (:family "Courier" :slant normal :weight normal :height 120 :width normal :antialias true)))))
 
 ;; swith header/cpp
 (add-hook 'c-mode-common-hook
