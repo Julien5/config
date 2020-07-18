82ae7b58db88d58dfc7aefe35d4d0d27d353085c
Julien5 <julien_bourgeois@yahoo.fr>
2020-07-18 16:17:50 +0200

    * remove automatic garbage

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -82,23 +82,3 @@
 
 (jbo-setup-windows)
 (message "hi")
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (wombat)))
- '(package-selected-packages
-   (quote
-	(smartscan magit expand-region company-fuzzy ag projectile company)))
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Courier" :slant normal :weight normal :height 120 :width normal :antialias true)))))
