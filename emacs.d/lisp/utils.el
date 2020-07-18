97c2d6301a705fa2948c7d73f4baa11faf8b0646
Julien5 <julien_bourgeois@yahoo.fr>
2020-07-18 02:34:02 +0200

    * fixing project roots

--- emacs.d/lisp/utils.el
+++ emacs.d/lisp/utils.el
@@ -1,3 +1,14 @@
+(defun jbo-projectiles ()
+  (setq cmd (format "cat %s" (shell-quote-argument (expand-file-name "~/.op/projectiles"))))
+  (setq R (split-string (shell-command-to-string cmd)))
+  (setq D (list))
+  (dolist (r R)
+	(setq D (cons (file-name-as-directory r) D))
+	)
+  D
+  )
+
+
 (defun update-tags-for-file-execute (filename)
   (setq executable (format "%s/bash/%s" user-emacs-directory "update-tags.sh"))
   (setq cmd (format "%s %s" executable
@@ -164,27 +175,29 @@
 			  (infer-indentation-style)))
   )
 
-(defvar project-root-markers '(".projectile")
-  "Files or directories that indicate the root of a project.")
+(defun jbo/project-try (dir)
+  (message "directory:%s" dir)
+  (cons 'jbo dir)
+  )
 
-(defun aorst/project-find-root (path)
-  "Tail-recursive search in PATH for root markers."
-  (let* ((this-dir (file-name-as-directory (file-truename path)))
-		 (parent-dir (expand-file-name (concat this-dir "../")))
-		 (system-root-dir (expand-file-name "/")))
-	(cond
-	 ((aorst/project-root-p this-dir) (cons 'transient this-dir))
-	 ((equal system-root-dir this-dir) nil)
-	 (t (aorst/project-find-root parent-dir)))))
+(cl-defmethod project-roots ((project (head jbo)))
+  ;;(setq R (list "a/" "b/"))
+  (setq R (jbo-projectiles))
+  (message "R:%s" R)
+  (setq D (cdr project))
+  (message "D:%s" D)
+  (setq R (append R (list D)))
+  ;;(setq R (list "a/" "b/" (cdr project)))
+  (message "project:%s" project)
+  (message "R:%s" R)
+  ;; (delete-dups )
 
-(defun aorst/project-root-p (path)
-  "Check if current PATH has any of project root markers."
-  (let ((results (mapcar (lambda (marker)
-						   (file-exists-p (concat path marker)))
-						 project-root-markers)))
-	(eval `(or ,@ results))))
+  (delete-dups R)
+  )
 
 (defun jbo-fix-project-roots ()
   (setq project-find-functions nil)
-  (add-to-list 'project-find-functions #'aorst/project-find-root)
+  (add-hook 'project-find-functions 'jbo/project-try)
   )
+
+(jbo-fix-project-roots)
