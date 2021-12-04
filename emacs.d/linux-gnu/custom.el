5fb31980c61e3423348f3221e585131d37a0abfb
Julien5 <julien_bourgeois@yahoo.fr>
2021-12-04 08:28:29 +0100

    linux font

---
+++ emacs.d/linux-gnu/custom.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(blink-cursor-mode nil)
+ '(cua-mode t nil (cua-base))
+ '(custom-enabled-themes '(deeper-blue))
+ '(package-selected-packages
+   '(use-package lsp-mode flycheck smartscan magit expand-region company-fuzzy ag projectile company))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :slant normal :weight light :height 137 :width normal :antialias true :foundry "ADBO")))))
+
+(set-face-attribute 'mode-line nil :font "Source Code Pro-12")
+
+
