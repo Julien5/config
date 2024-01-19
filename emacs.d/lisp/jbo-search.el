c84ac36b8596f425b0aed7304fe77a3039868bd5
Julien Bourgeois <julien_bourgeois@yahoo.fr>
2024-01-19 07:48:22 +0100

    add filename with no extension to ag regexp (case "update" script)

--- emacs.d/lisp/jbo-search.el
+++ emacs.d/lisp/jbo-search.el
@@ -16,7 +16,7 @@
 	(if word
 		(let ((w word))
 		  (require 'ag)
-		  (ag/search w jbo-dir :file-regex ".cpp$|.c$|.h$|.sh$|.py$|.pro$|.el|Jenkinsfile$")
+		  (ag/search w jbo-dir :file-regex ".cpp$|.c$|.h$|.sh$|.py$|.pro$|.el|Jenkinsfile$|[^.]*$")
 		  )
 	  (message "no symbol at cursor")
 	  )
