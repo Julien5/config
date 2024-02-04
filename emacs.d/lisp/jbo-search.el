267b53b4448dd575262222b4b11ef89742c67489
Julien5 <julien_bourgeois@yahoo.fr>
2024-02-04 08:42:37 +0100

    jbo/other-file with automatic disambiguation

--- emacs.d/lisp/jbo-search.el
+++ emacs.d/lisp/jbo-search.el
@@ -1,3 +1,5 @@
+;; -*- lexical-binding: t -*-
+
 (defun get-identifier-to-search ()
   (setq jbo--region nil)
   (if (use-region-p)
@@ -54,13 +56,6 @@
 	(jbo--find-definition-at-point))
   )
 
-;; TODO: filter the ouput from
-;; (projectile-get-other-files (buffer-file-name))
-(defun jbo/other-file ()
-  (interactive)
-  (projectile-find-other-file)
-  )
-
 (defun jbo/projectile-ag ()
   (interactive)
   (setq jboword (read-string "ag: " (get-identifier-to-search)))
@@ -69,5 +64,52 @@
   ;;(switch-to-buffer "main_devhost.cpp")
   ;;(refactor-references)
   ;;(switch-to-buffer "search.el")
+
+
+(defun file-distance (f1 f2)
+  (defun common (A B)
+	(let ((a (car A))
+		  (b (car B))
+		  )
+	  (if (and (string= a b) (and a b))
+		  (+ (common (cdr A) (cdr B)) 1)
+		0
+		)
+	  )
+	)
+  
+  (let ((L1 (split-string (file-name-directory f1) "/"))
+		(L2 (split-string (file-name-directory f2) "/"))
+		)
+	(let ((ret (common L1 L2)))
+	  (message "f1=%s f2=%s distance=%d" f1 f2 ret)
+	  ret
+	  ))
+  )
+
+(eval-when-compile
+  (require 'cl-lib))
+
+(defun jbo/other-file ()
+  (interactive)
+  (setq f (file-relative-name buffer-file-name (projectile-project-root)))
+  (setq L (projectile-get-other-files f))
+  (unless L (error "No other file found for %s" f))
+  (setq Q nil)
+  (dolist (o L)
+	(let ((d (file-distance f o)))
+	  (if (not Q)
+		  (setq Q (list (cons o d)))
+		(setq Q (append (list (cons o d)) Q))
+		)
+	  )
+	)
+  (sort Q (lambda (od1 od2) (> (cdr od1) (cdr od2))))
+  (setq rel (car (car Q)))
+  (setq abs (concat (projectile-project-root) rel))
+  (message "list=%s winner=%s" L abs)
+  (find-file abs)
+  )
+
 
 (message "loaded jbo-search")
