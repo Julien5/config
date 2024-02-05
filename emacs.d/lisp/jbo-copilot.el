75c595a713b3873f51deb7dfadd5276f7cf09c97
Julien5 <julien_bourgeois@yahoo.fr>
2024-02-05 06:18:43 +0100

    add jbo-copilot.el

---
+++ emacs.d/lisp/jbo-copilot.el
@@ -0,0 +1,5 @@
+(setq copilot-node-executable "/opt/copilot/node.js/node-v20.11.0-linux-x64/bin/node")
+(add-to-list 'load-path "~/.emacs.d/lisp/copilot/copilot.el")
+;;(load "~/emacs.d/lisp/copilot/copilot.el/copilot.el")
+(require 'copilot)
+(message "ok")
