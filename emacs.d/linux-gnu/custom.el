801b124eb3c016e452fee4029e622e0513dd6c49
Julien5 <julien_bourgeois@yahoo.fr>
2024-10-19 07:41:05 +0200

    save options

--- emacs.d/linux-gnu/custom.el
+++ emacs.d/linux-gnu/custom.el
@@ -3,11 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(cua-mode t)
+ '(custom-enabled-themes '(deeper-blue))
+ '(package-selected-packages nil)
+ '(tool-bar-mode nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "TamzenForPowerline" :foundry "UNKN" :slant normal :weight medium :height 119 :width normal)))))
