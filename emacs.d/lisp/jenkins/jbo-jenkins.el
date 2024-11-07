49004a58da6e683e6236c8c85eff728107cc32c0
Julien Bourgeois <julien_bourgeois@yahoo.fr>
2024-11-07 16:54:26 +0100

    update

--- emacs.d/lisp/jenkins/jbo-jenkins.el
+++ emacs.d/lisp/jenkins/jbo-jenkins.el
@@ -1,4 +1,7 @@
-(add-to-list 'load-path "~/.emacs.d/lisp/jenkins/jenkins.el-master")
-(require 'jenkins)
-(load "~/.ssh/github-work/jbo-jenkins.el")
-
+(if (file-exists-p "~/.ssh/github-work/jbo-jenkins.el")
+	(progn
+	  (add-to-list 'load-path "~/.emacs.d/lisp/jenkins/jenkins.el-master")
+	  (require 'jenkins)
+	  (load "~/.ssh/github-work/jbo-jenkins.el")
+	  )
+  )
