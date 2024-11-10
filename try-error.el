dc822e733becf02c8289ba7fe6533fefe7b8850d
Julien Bourgeois <julien_bourgeois@yahoo.fr>
2024-11-10 14:10:29 +0100

    wip

--- try-error.el
+++ try-error.el
@@ -1,17 +1,36 @@
+;;; -*- lexical-binding: t -*-
 
-(defun loc-worker ()
+(define-error 'stop-signal "stop" 'stop-continue-signals)
+(define-error 'continue-signal "continue" 'stop-continue-signals)
+
+(defun loc-worker-1 (ok)
   (message "worker-start")
-  (error "bang")
+
+  (if (not ok)
+	  (signal 'stop-signal "stop-1")
+	)
+  
   (message "worker-end")
   )
 
-
-(defun loc-main ()
-  (condition-case _
-	  (loc-worker)
-	(error (message "error found:%s" _))
-	)
+(defun loc-worker-2 ()
+  (message "worker-start")
+  (signal 'stop-signal "stop-2")
+  (message "worker-end")
   )
 
+(defun loc-main ()
+  (let ((code "OK"))
+
+	(condition-case _ (progn (loc-worker-1 1)
+							 (signal 'continue-signal "ok")
+							 )
+	  (stop-signal (message "stop found:%s" _))
+	  (continue-signal (message "continue found:%s" _))
+	  )
+	(message "code=%s" code)
+	
+	)
+  )
 
 (loc-main)
