a8f7d717a0d4dc735c271f2e0ddd05837fedeec6
Julien Bourgeois <julien_bourgeois@yahoo.fr>
2024-11-11 12:59:57 +0100

    f4 in markdown => follow-link-at-point

--- try-error.el
+++ try-error.el
@@ -19,6 +19,22 @@
   (message "worker-end-2")
   )
 
+(defun loc-worker-3 (ok)
+  (message "worker-start-3")
+  (if (not ok)
+	  (throw 'failed "loc-3")
+	)
+  (message "worker-end-3")
+  )
+
+(defun loc-worker-4 (ok)
+  (message "worker-start-4")
+  (if (not ok)
+	  (user-error "loc-4")
+	)
+  (message "worker-end-4")
+  )
+
 (defun loc-main ()
   (condition-case _ (progn (loc-worker-1 'nil)
 						   (signal 'stop-signal "stop")
@@ -33,6 +49,18 @@
 						   )
 	(stop-signal (message "stop found:%s" _)(error "done"))
 	(continue-signal (message "continue found:%s" _))
+	)
+
+  (catch 'failed
+	(loc-worker-3 nil)
+	(error "done"))
+
+  (condition-case _ (progn
+					  (loc-worker-4 'nil)
+					  (signal 'stop-signal "stop")
+					  )
+	(stop-signal (message "stop found:%s" _)(error "done"))
+	(user-error (message "continue found:%s" _))
 	)
 
   (error "failed")
