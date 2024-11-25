42f3de4bc35e3e7b4ae399c4c2ea68fa523badaa
Julien5 <julien_bourgeois@yahoo.fr>
2024-11-25 06:02:57 +0100

    update

--- profile/profile.avr.sh
+++ profile/profile.avr.sh
@@ -3,7 +3,7 @@
 export PATH=$PATH:/opt/avr/avr-gcc-14.1.0-x64-linux/bin/:/opt/avr/avrdude_Linux_64bit/bin/
 
 function screen () {
-	/usr/bin/screen /dev/ttyUSB0 9600
+	/usr/bin/screen -L /dev/ttyUSB0 9600
 }
 
 
