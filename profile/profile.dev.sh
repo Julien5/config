bb860e48cf079a603779a7f6152e86b2f9163154
Julien5 <julien_bourgeois@yahoo.fr>
2024-12-20 09:19:02 +0100

    fix esp8266 paths

--- profile/profile.dev.sh
+++ profile/profile.dev.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
-function dev.esp8266.open.rtos() {
-	initpath;
-    DIR=/opt/esp8266/esp8266-toolchain
-    export PATH=$PATH:$DIR/compiler/xtensa-lx106-elf/bin/
-}
-
 function dev.esp8266() {
 	initpath;
-    local DIR=/opt/esp8266-toolchain/
+    local DIR=/opt/esp8266-toolchain
     export PATH=$PATH:$DIR/xtensa-lx106-elf/bin
     export IDF_PATH=$DIR/ESP8266_RTOS_SDK
     export SDK_PATH=$DIR/ESP8266_RTOS_SDK
 	export PYTHONPATH=/opt/esp8266-toolchain/python-dependencies/
+	# do not setup compile database for esp8266 builds
+	# clangd fails to *not include* host dirs like /usr/include.
 }
 
 function dev.avr() {
 	initpath
 	echo sourcing $JULIEN5CONFIGPATH/profile/profile.avr.sh
 	. $JULIEN5CONFIGPATH/profile/profile.avr.sh
+}
+
+
+function dev.pc() {
+	initpath
+	mkdir -p /tmp/builds/PC/
+	rm -f /tmp/builds/build
+	ln -s /tmp/builds/PC /tmp/builds/build
 }
