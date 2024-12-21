1cb48a389813176f708b8e6bd797735a2ffa5780
Julien5 <julien_bourgeois@yahoo.fr>
2024-12-21 22:58:10 +0100

    mount-android script, based on go-mtpfs

--- profile/profile.julien.sh
+++ profile/profile.julien.sh
@@ -7,3 +7,31 @@ function open-notes() {
 function copy() {
 	xclip -selection c
 }
+
+function mount-android() {
+	# try `-android=0` if it fails with sony xperia.
+	local DIRNAME=/tmp/android-pixel-mount
+	mkdir -p ${DIRNAME}
+	umount ${DIRNAME} || true
+	echo mounting...
+	go-mtpfs ${DIRNAME} &
+	sleep 1
+	if ! grep -qs "${DIRNAME} " /proc/mounts &> /dev/null; then
+		echo "failed to mount ${DIRNAME}"
+		echo "check:"
+		echo "    - lsusb"
+		return 1
+	fi
+	if ! find ${DIRNAME}  -mindepth 1 -maxdepth 1 &> /dev/null; then
+		echo "mounted ${DIRNAME} but failed to read."
+		echo "maybe access to device is not granted"
+		return 1
+	fi
+	W=$(find ${DIRNAME}  -mindepth 1 -maxdepth 1)
+	if [ -z ${W} ]; then
+		echo "mounted ${DIRNAME} but failed to read any file."
+		echo "maybe access to device is not granted"
+		return 1
+	fi
+	xdg-open ${DIRNAME}
+}
