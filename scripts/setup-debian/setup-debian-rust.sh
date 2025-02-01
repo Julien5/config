09ffd91b534e9d77a0a4287b084fbb894be59bd8
Julien5 <julien_bourgeois@yahoo.fr>
2025-02-01 10:26:16 +0100

    rust install in /opt/rust

--- scripts/setup-debian/setup-debian-rust.sh
+++ scripts/setup-debian/setup-debian-rust.sh
@@ -3,16 +3,29 @@
 set -e
 set -x
 
-function install() {
-	DESTDIR=/opt/rust
+DESTDIR=/opt/rust
+
+function install-analyzer() {
+	local D=${DESTDIR}/analyzer
+	mkdir -p ${D}/bin
+	local URL=https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz
+	curl -L "${URL}" | gunzip -c - > ${D}/bin/rust-analyzer
+	chmod +x ${D}/bin/rust-analyzer
+}
+
+function install-compiler() {
 	mkdir -p $DESTDIR
-	export CARGO_HOME=$DESTDIR/cargo
-	export RUSTUP_HOME=$DESTDIR/rustup
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
+	if [ ! -e /opt/rust/rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/rustc ]; then
+		export CARGO_HOME=$DESTDIR/cargo
+		export RUSTUP_HOME=$DESTDIR/rustup
+		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
+	fi
 }
 
 function main() {
-	install
+	install-compiler
+	install-analyzer
+	echo done
 }
 
 main "$@"
